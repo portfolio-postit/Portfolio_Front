27fb487b5ba32476d3927cb77037666c5e7cfeea
jun <toinin@naver.com>
2021-04-05 23:21:01 +0900

    Add postit(SubPostit, GarabagePostit , ProjectPostit)

--- src/components/home/body/body.js
+++ src/components/home/body/body.js
@@ -15,21 +15,34 @@ const Box = styled.div`
   }
 `
 const LeftBox = styled.div`
-    display:grid;
+    display:flex;
     height:100%;
     width:100%;
-    background:red;
+    flex-direction: column;
+
     @media only screen and (max-width: 768px) {
-    display:flex;
     height:0;
-    flex-direction: column
   }
 `
+
 const RightBox = styled.div`
-    display:grid;
+    display:flex;
     height:100%;
     width:100%;
-     @media only screen and (max-width: 768px) {
+    
+    .ProjectPostit{ 
+        @media only screen and (max-width: 768px) {
+        margin-top:calc(40.5vw);
+        margin-left:3rem;
+        position: absolute;
+        left:22%;
+        display:flex;
+        height:calc(65vw);
+        width:40%;
+        background-size:cover;
+        background-image: url("projectPostit.png");
+    }}
+    @media only screen and (max-width: 768px) {
     display:flex;
     flex-direction: column
   }
@@ -41,7 +54,6 @@ const MainPostit = styled.div`
   	transform: rotate( 5deg );
     height:calc(30.5vw);
     width:36%;
-
     display:flex;
     flex-direction:column;
     background-size:contain;
@@ -64,13 +76,64 @@ const MainPostit = styled.div`
     width:55%;
     }
     `
+const ProjectPostit = styled.div`
+    margin-left:7%;
+    display:flex;
+    width:80%;
+    height:calc(32vw);
+    background-repeat:no-repeat;
+    background-size:cover; 
+    background-image: url("projectPostit.png");
+`
 
+const GarabagePostit = styled.div`
+  	transform: rotate( 5deg );
+    justify-content:center;
+    align-items:center;
+    line-height:center;
+    margin-top:2%;
+    height:calc(7vw);
+    width:66%;
+    display:flex;
+    background-repeat:no-repeat;
+    background-size:cover;
+    background-image: url("garabagePostit.png");
+    margin-left:20%;
+    
+    p{
+        margin-top:1rem;
+        transform: rotate( 5deg );
+        width: 47%;
+        font-size:0.9rem;
+    }
+    p:after {
+        content: "";
+        display: block;
+        width: 7rem;
+        border-bottom: 1px solid #707070;
+        margin: 10px auto;
+}
+`
+const SubPostit = styled.div`
+    display:flex;
+    height:calc(13vw);
+    width:23%;
+    background-size:cover;
+    background-image: url("subPostit.png");
+    margin-top:2%;
+    margin-left:4%;; 
+    &:nth-child(3){
+    margin-top:3%;
+    margin-left:13%;
+}
+`
 const Body = () => {
     return (
         <>
             <Box>
                 <LeftBox>
-
+                    <GarabagePostit />
+                    <ProjectPostit />
                 </LeftBox>
                 <RightBox>
                     <MainPostit>
@@ -78,6 +141,11 @@ const Body = () => {
                         <p> YeoungJun</p>
                         <p> Ahn : 안영준</p>
                     </MainPostit>
+                    <SubPostit/>    
+                    <SubPostit/>
+
+                    <div class="ProjectPostit" />
+
                 </RightBox>
             </Box>
         </>
