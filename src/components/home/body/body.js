c70b48bec1e3fb449976f957a785d5111aa4e336
jun <toinin@naver.com>
2021-04-12 23:45:54 +0900

    Test  axios body

--- src/components/home/body/body.js
+++ src/components/home/body/body.js
@@ -1,5 +1,41 @@
+import React, { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import * as S from "./style";
+import { MAIN } from "../../../lib/API";
+import { requestGetApi } from "../../../lib/REQUEST";
+import { getMainSaga } from "../../../modules/redux/main";
+import axios from "axios";
+
 const Body = () => {
+  // const res = await axios.patch(
+  //   "http://52.78.215.67:8080/user",
+  //   {},
+  //   {
+  //     headers: {
+  //       Authorization:accessToken
+  //     },
+  //   }
+  // );
+  // console.log(res.status);
+
+  // const email = useSelector((state) => state.main.main.about);
+  const email = useSelector((state) => state.main.main?.about?.email);
+  const name = useSelector((state) => state.main.main?.about?.username);
+  // const [tool, tool2] = useSelector((state) => state.main.main?.tool);
+  const tool = useSelector((state) => state.main.main?.tool);
+  const dispatch = useDispatch();
+
+  const [tool1, tool2] = tool || ["", ""];
+
+  const tool12 = process.env.REACT_APP_S3_URL + tool1.file_name;
+  console.log(tool12);
+  const initMainSaga = useCallback(() => {
+    dispatch(getMainSaga());
+  }, [dispatch]);
+
+  useEffect(() => {
+    initMainSaga();
+  }, []);
   return (
     <>
       <S.Box>
@@ -7,13 +43,13 @@ const Body = () => {
           <S.GarabagePostit>
             <p>useless thoughts</p>
           </S.GarabagePostit>
-          <S.ProjectPostit />
+          <S.ProjectPostit></S.ProjectPostit>
         </S.LeftBox>
         <S.RightBox>
           <S.MainPostit>
             <p> HI I'm</p>
             <p> YeoungJun</p>
-            <p> Ahn : 안영준</p>
+            <p>{name || "SEX"}</p>
           </S.MainPostit>
           <S.SubPostit>
             <p>click on postit</p>
@@ -22,8 +58,8 @@ const Body = () => {
             <p>sense of responsibility</p>
           </S.SubPostit>
           <S.SKillPostit>
-            <p>FrameWork</p>
-            <img src="react.png" class="size margin1" />
+            <p>Tool</p>
+            <img src={tool12} class="size margin1" />
             <img
               src="springboot.png"
               class="margin2"
@@ -66,7 +102,7 @@ const Body = () => {
             <S.AboutComment>010-7670-3833</S.AboutComment>
             <S.AboutComment>ahn479512@gmail.com</S.AboutComment>
           </S.AboutPost>
-          <div class="ProjectPostit" />
+          <div class="ProjectPostit"> </div>
         </S.RightBox>
       </S.Box>
     </>
