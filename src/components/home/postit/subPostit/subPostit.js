72cd740fc07689dd613dc6514b81f301ba9cea75
jun <toinin@naver.com>
2021-04-13 11:16:23 +0900

    Add components - subpostit

---
+++ src/components/home/postit/subPostit/subPostit.js
@@ -0,0 +1,16 @@
+import * as S from "./style";
+
+const subPostit = () => {
+  return (
+    <>
+      <S.SubPostit>
+        <p>click on postit</p>
+      </S.SubPostit>
+      <S.SubPostit>
+        <p>sense of responsibility</p>
+      </S.SubPostit>
+    </>
+  );
+};
+
+export default subPostit;
