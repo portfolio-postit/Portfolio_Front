4d9972742facf1b54bfd81a16652548e4802e642
jun <toinin@naver.com>
2021-04-14 23:46:33 +0900

    Fix style - projectpostit

--- src/components/home/postit/projectPostit/style/index.js
+++ src/components/home/postit/projectPostit/style/index.js
@@ -4,7 +4,7 @@ export const ProjectPostit = styled.div`
   margin-left: 7%;
   display: flex;
   flex-direction: column;
-  width: 80%;
+  width: 19.6vw;
   height: calc(32vw);
   background-repeat: no-repeat;
   background-size: cover;
@@ -18,5 +18,19 @@ export const ProjectPostit = styled.div`
     margin-top: 15%;
     margin-left: 38%;
     height: 10%;
+    @media only screen and (max-width: 768px) {
+      margin-top: 17%;
+      font-weight: 1000;
+      font-size: 3.5vw;
+      margin-left: 33%;
+    }
+  }
+
+  @media only screen and (max-width: 768px) {
+    width: 40vw;
+    height: 83vw;
+    margin-left: 35%;
+    margin-top: 45%;
+    font-size: calc(1.8vw);
   }
 `;
