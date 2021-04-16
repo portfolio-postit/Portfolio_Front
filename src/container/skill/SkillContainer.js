d14d8a983669fca5c4a0e8721ed3c344113eb9d7
jun <toinin@naver.com>
2021-04-16 10:40:29 +0900

    Add container - skill - base

---
+++ src/container/skill/SkillContainer.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import HeaderContainer from "../header/headerContainer";
+
+const SkillContainer = () => {
+  const A = styled.div`
+    width: 100%;
+    height: 100%;
+    background: black;
+  `;
+
+  return (
+    <>
+      <HeaderContainer />
+    </>
+  );
+};
+export default SkillContainer;
