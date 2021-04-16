fbbe87b1badfdeaebc7e51b10e57467278ee723c
jun <toinin@naver.com>
2021-04-16 10:40:58 +0900

    Add routing - base

--- src/Routing.js
+++ src/Routing.js
@@ -3,7 +3,8 @@ import { Route, Switch } from "react-router-dom";
 // import ModalWrapContainer from "./container/ModalWrap/ModalWrapContainer";
 import HomeContainer from "./container/home/HomeContainer";
 import DesktopRouting from "./container/desktop/DesktopRouting";
-import AboutContainer from "./container/about/AboutContainer.js";
+import AboutContainer from "./container/about/AboutContainer";
+import SkillContainer from "./container/skill/SkillContainer";
 const Routing = () => {
   return (
     <>
@@ -11,7 +12,7 @@ const Routing = () => {
         <Route path="/" exact component={DesktopRouting} />
         <Route path="/home" exact component={HomeContainer} />
         <Route paht="/about" component={AboutContainer} />
-        {/* <Route path="/skill" component={SkillContainer} /> */}
+        <Route path="/skill" component={SkillContainer} />
         {/* <Route path="/project" component={ProjectContainer} /> */}
       </Switch>
       {/* <ModalWrapContainer /> */}
