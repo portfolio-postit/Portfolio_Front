5bbe37cb6bdd924e6ae5d2a902ac8bee7dfdcfd2
jun <toinin@naver.com>
2021-04-16 16:36:42 +0900

    Add style - project wrapper

---
+++ src/components/project/style/index.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+export const Wrapper = styled.div`
+  height: calc(90rem);
+  width: calc(100% - 20px);
+  display: flex;
+  background: #a6d5ff;
+  flex-direction: column;
+  border-left: 10px solid #1a0c0c;
+  border-right: 10px solid #1a0c0c;
+`;
+
+export const ProjectList = styled.div`
+  height: 60vw;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  @media only screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`;
