d695a4f82c62995dee1216abce88ce3dc554702c
jun <toinin@naver.com>
2021-04-16 10:49:42 +0900

    Fix components header folder

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,88 +1,14 @@
-import styled from "styled-components";
-import { Route, Link } from "react-router-dom";
+import * as S from "./style";
 
-const Box = styled.div`
-  height: 7%;
-  width: 100%;
-  background: #1a0c0c;
-  display: flex;
-  /* text-align: center; */
-  @media only screen and (max-width: 768px) {
-    height: 80px;
-    display: inline;
-  }
-`;
-const CustomLink = styled(Link)`
-  margin-bottom: 1px;
-  background-color: #1a0c0c;
-  display: flex;
-  margin-top: 1.1rem;
-  text-decoration: none;
-  margin-left: 3rem;
-  color: white;
-  border: none;
-  outline: none;
-  font-weight: bold;
-  font-size: 1rem;
-  &:hover {
-    color: #b6b6b6;
-    cursor: pointer;
-  }
-  @media only screen and (max-width: 768px) {
-    font-size: 1rem;
-    height: 20px;
-    margin-top: 1.5px;
-    width: 100%;
-    margin-left: 0;
-    outline: none;
-    border: none;
-    padding-left: 46%;
-    padding-bottom: unset;
-    padding-top: 1%;
-  }
-  &:nth-child(3) {
-    @media only screen and (max-width: 768px) {
-      padding-left: 45.5%;
-    }
-  }
-  &:nth-child(4) {
-    @media only screen and (max-width: 768px) {
-      padding-left: 46.5%;
-    }
-  }
-  &:nth-child(5) {
-    @media only screen and (max-width: 768px) {
-      padding-left: 44.7%;
-    }
-  }
-`;
-
-const Title = styled.div`
-  margin-left: 4rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: white;
-  font-weight: bold;
-  font-style: italic;
-  background: #1a0c0c;
-  @media only screen and (max-width: 768px) {
-    font-size: 3.5vw;
-    width: 100%;
-    height: 50px;
-    margin-left: 0;
-    padding: unset;
-  }
-`;
 const Header = () => {
   return (
-    <Box>
-      <Title>YeoungJun Ahn : 안영준</Title>
-      <CustomLink to="/home">HOME</CustomLink>
-      <CustomLink to="/about">ABOUT</CustomLink>
-      <CustomLink to="/skill">SKILL</CustomLink>
-      <CustomLink to="/project">PROJECT</CustomLink>
-    </Box>
+    <S.Box>
+      <S.Title>YeoungJun Ahn : 안영준</S.Title>
+      <S.CustomLink to="/home">HOME</S.CustomLink>
+      <S.CustomLink to="/about">ABOUT</S.CustomLink>
+      <S.CustomLink to="/skill">SKILL</S.CustomLink>
+      <S.CustomLink to="/project">PROJECT</S.CustomLink>
+    </S.Box>
   );
 };
 
