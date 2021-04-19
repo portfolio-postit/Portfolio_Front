15949d4173437db9a9eb98b34a29527211429864
jun <toinin@naver.com>
2021-04-19 23:33:34 +0900

    Add redux - about

--- src/modules/redux/index.js
+++ src/modules/redux/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 import main from "./main";
 import modal from "./modal";
-
+import about from "./about";
 const rootReducer = combineReducers({
   main,
+  about,
   modal,
 });
 
