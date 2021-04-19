af6c451248cecf88f8d949d5c7df7b3e823c535a
jun <toinin@naver.com>
2021-04-19 15:18:35 +0900

    Add components - bottomWrapper

---
+++ src/components/about/BottomWrapper/BottomWrapper.js
@@ -0,0 +1,17 @@
+import * as S from "./style";
+const BottomWrapper = () => {
+  return (
+    <S.BottomWrapper>
+      <S.Image src="https://image.shutterstock.com/image-photo/competition-forest-trees-sunlight-necessary-260nw-1186179847.jpg" />
+      <S.Content>
+        <div>안영준</div>
+        <div>이메일</div>
+        <div>깃허브</div>
+        <div>전화번호</div>
+        <div>학력</div>
+      </S.Content>
+    </S.BottomWrapper>
+  );
+};
+
+export default BottomWrapper;
