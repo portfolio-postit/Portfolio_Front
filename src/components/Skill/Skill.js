06e690e3ea2b2c5e1a86b386f2599b2c3a0a70ee
jun <toinin@naver.com>
2021-04-19 15:17:31 +0900

    Add components - skill base

---
+++ src/components/Skill/Skill.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+const Skill = () => {
+  const Wrapper = styled.div`
+    height: 110%;
+    width: 100%;
+    background: #a6d5ff;
+    display: flex;
+  `;
+  return <Wrapper>s</Wrapper>;
+};
+export default Skill;
