f6f2b669c29d5f8ec818f821ac42a148dffeb6f8
jun <toinin@naver.com>
2021-04-19 23:33:10 +0900

    Add saga - about

--- src/modules/saga/index.js
+++ src/modules/saga/index.js
@@ -1,9 +1,9 @@
 import { all } from "redux-saga/effects";
 // import authSaga from "./auth";
 import mainSaga from "./main";
-
+import aboutSaga from "./about";
 function* rootSaga() {
-  yield all([mainSaga()]);
+  yield all([mainSaga(), aboutSaga()]);
 }
 
 export default rootSaga;
