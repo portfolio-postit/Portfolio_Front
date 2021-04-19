4439340d732315df7900b6b9a2cd5e76e707c756
jun <toinin@naver.com>
2021-04-19 11:45:44 +0900

    Add container - about

--- src/container/about/AboutContainer.js
+++ src/container/about/AboutContainer.js
@@ -1,16 +1,11 @@
 import styled from "styled-components";
 import HeaderContainer from "../header/headerContainer";
-
+import About from "../../components/about/about";
 const AboutContainer = () => {
-  const A = styled.div`
-    width: 100%;
-    height: 100%;
-    background: black;
-  `;
-
   return (
     <>
       <HeaderContainer />
+      <About />
     </>
   );
 };
