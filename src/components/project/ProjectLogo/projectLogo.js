d2192a070bc807cc54799f99956e9f9131b10cc6
jun <toinin@naver.com>
2021-04-19 11:05:17 +0900

    Add components - project - logo

--- src/components/project/ProjectLogo/projectLogo.js
+++ src/components/project/ProjectLogo/projectLogo.js
@@ -2,9 +2,14 @@ import * as S from "./style";
 
 const ProjectLogo = () => {
   return (
-    <S.ProjectLogo>
-      <p>PROJECT</p>
-    </S.ProjectLogo>
+    <S.Wrapper>
+      <S.ProjectLogo>
+        <p>PROJECT</p>
+      </S.ProjectLogo>
+      <S.HomeLogo>
+        <p>HOME</p>
+      </S.HomeLogo>
+    </S.Wrapper>
   );
 };
 export default ProjectLogo;
