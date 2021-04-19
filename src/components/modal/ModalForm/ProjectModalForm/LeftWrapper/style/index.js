10fbbb2ddf2dddbd2945cfff046aa5a7075c26c2
jun <toinin@naver.com>
2021-04-19 10:57:43 +0900

    Add style - leftwrapper

---
+++ src/components/modal/ModalForm/ProjectModalForm/LeftWrapper/style/index.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const LeftWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Image = styled.img`
+  height: 20vw;
+  width: 30vw;
+  margin-left: 5vw;
+  margin-top: 1vw;
+`;
+export const Content = styled.div`
+  height: 2vw;
+  margin-top: 7vw;
+  font-size: 1.5vw;
+  font-weight: 700;
+  margin-left: 5vw;
+`;
+export const Link = styled.a`
+  margin-left: 6vw;
+  font-size: 1vw;
+`;
