288227fc0df831d8d33ae0a08063eca91c8c867b
jun <toinin@naver.com>
2021-04-19 15:16:54 +0900

    Fix components about

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,39 +1,15 @@
 import * as S from "./style";
+import TopWrapper from "./TopWrapper/TopWrapper";
+import BottomWrapper from "./BottomWrapper/BottomWrapper";
+import Qustion from "./Qustion/Question";
 const About = () => {
   return (
     <S.Wrapper>
       <S.LeftWrapper>
-        <S.TopWrapper>
-          <S.ProjectLogo>
-            <p>ABOUT</p>
-          </S.ProjectLogo>
-          <S.HomeLogo>
-            <p>HOME</p>
-          </S.HomeLogo>
-        </S.TopWrapper>
-        <S.BottomWrapper>
-          <S.Introduction>
-            <S.Image src="https://image.shutterstock.com/image-photo/competition-forest-trees-sunlight-necessary-260nw-1186179847.jpg" />
-            <S.Content>
-              <div>안영준</div>
-              <div>이메일</div>
-              <div>깃허브</div>
-              <div>전화번호</div>
-              <div>학력</div>
-            </S.Content>
-          </S.Introduction>
-        </S.BottomWrapper>
+        <TopWrapper />
+        <BottomWrapper />
       </S.LeftWrapper>
-      <S.Qustion>
-        <S.Questiontitle>Question</S.Questiontitle>
-        <S.QuestionContent>Username or email address</S.QuestionContent>
-        <S.EmailInput />
-        <S.QuestionContent>Content</S.QuestionContent>
-        <S.ContentInput />
-        <S.SendBtn>
-          <div>SEND</div>
-        </S.SendBtn>
-      </S.Qustion>
+      <Qustion />
     </S.Wrapper>
   );
 };
