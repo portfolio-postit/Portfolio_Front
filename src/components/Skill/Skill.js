a02f4e7dd0b0104afac17523d39b265fcc227789
jun <toinin@naver.com>
2021-04-19 17:17:20 +0900

    Add style - topwrapper - base

--- src/components/Skill/Skill.js
+++ src/components/Skill/Skill.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import BottomWrapper from "./BottomWrapper/BottomWrapper";
+import TopWrapper from "./TopWrapper/TopWrapper";
+import SkillItem from "./SkillItem/SkillItem";
 const Skill = () => {
   const Wrapper = styled.div`
     height: 110%;
@@ -6,6 +9,36 @@ const Skill = () => {
     background: #a6d5ff;
     display: flex;
   `;
-  return <Wrapper>s</Wrapper>;
+  const LeftWrapper = styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+  `;
+
+  const SkillList = styled.div`
+    margin-top: 3vw;
+    margin-left: 2vw;
+    height: 30vw;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    @media only screen and (max-width: 768px) {
+      grid-template-columns: 1fr;
+    }
+  `;
+
+  return (
+    <Wrapper>
+      <LeftWrapper>
+        <TopWrapper />
+        <BottomWrapper />
+      </LeftWrapper>
+      <SkillList>
+        <SkillItem />
+        <SkillItem />
+        <SkillItem />
+      </SkillList>
+    </Wrapper>
+  );
 };
 export default Skill;
