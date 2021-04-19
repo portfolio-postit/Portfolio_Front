d81c531459981dfd171f5876e758f7933cfae8c6
jun <toinin@naver.com>
2021-04-19 10:57:55 +0900

    Add modal - leftwrapper

---
+++ src/components/modal/ModalForm/ProjectModalForm/LeftWrapper/LeftWrapper.js
@@ -0,0 +1,14 @@
+import * as S from "./style";
+
+const LeftWrapper = () => {
+  return (
+    <S.LeftWrapper>
+      <S.Image src="https://image.shutterstock.com/image-photo/competition-forest-trees-sunlight-necessary-260nw-1186179847.jpg" />
+      <S.Content>관련 링크</S.Content>
+      <S.Link href="https://github.com/Ahnyeongjun" target="blank">
+        https://github.com/Ahnyeongjun
+      </S.Link>
+    </S.LeftWrapper>
+  );
+};
+export default LeftWrapper;
