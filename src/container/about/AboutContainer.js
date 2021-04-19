d517889a1d7ec761d078a49c5d7f51c351c5d5cb
jun <toinin@naver.com>
2021-04-19 23:35:06 +0900

    Add Aboutcontainer dispatch

--- src/container/about/AboutContainer.js
+++ src/container/about/AboutContainer.js
@@ -1,10 +1,23 @@
 import HeaderContainer from "../header/headerContainer";
 import About from "../../components/about/about";
+import React, { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAboutSaga } from "../../modules/redux/about";
 const AboutContainer = () => {
+  const about = useSelector((state) => state.about);
+  const dispatch = useDispatch();
+
+  const initMainSaga = useCallback(() => {
+    dispatch(getAboutSaga());
+  }, [dispatch]);
+
+  useEffect(() => {
+    initMainSaga();
+  }, []);
   return (
     <>
       <HeaderContainer />
-      <About />
+      <About about={about} />
     </>
   );
 };
