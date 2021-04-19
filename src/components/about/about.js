729484a7a5d1348361351d26b1794a010fbf1493
jun <toinin@naver.com>
2021-04-19 13:58:32 +0900

    Add components - base

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 const Wrapper = styled.div`
-  height: 97%;
+  height: 105%;
   width: 100%;
   background: #a6d5ff;
   display: flex;
@@ -10,9 +10,8 @@ const TopWrapper = styled.div`
 `;
 const ProjectLogo = styled.div`
   height: 12vw;
-  width: 30vw;
-  background-color: black;
-  /* background-image: url("projectTitle.png"); */
+  width: 27vw;
+  background-image: url("projectTitle.png");
   background-repeat: no-repeat;
   background-size: cover;
   margin-top: 1%;
@@ -33,19 +32,37 @@ const ProjectLogo = styled.div`
   }
 `;
 
-const Big = styled.div`
-  height: 30vw;
-  width: 60vw;
-  background-color: black;
-
-  /* background-image: url("big.png"); */
+const Introduction = styled.div`
+  height: 35.1vw;
+  width: 70vw;
+  background-image: url("content.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+`;
+const Qustion = styled.div`
+  margin-top: 8vw;
+  height: 40vw;
+  width: 25vw;
+  margin-left: 3vw;
+  background-image: url("question.png");
   background-repeat: no-repeat;
   background-size: cover;
 `;
-const Qustion = styled.div`
-  background-color: black;
-  height: 40vw;
-  width: 20vw;
+const Questiontitle = styled.div`
+  margin-top: 5.5vw;
+  margin-left: 7vw;
+  font-size: 2.5vw;
+  font-weight: 1000;
+  font-style: italic;
+  transform: rotate(2deg);
+  margin-bottom: 3vw;
+`;
+const QuestionContent = styled.div`
+  font-size: 1vw;
+  margin-top: 1vw;
+  margin-left: 3vw;
+  transform: rotate(2deg);
 `;
 const HomeLogo = styled.div`
   height: 12vw;
@@ -73,15 +90,85 @@ const HomeLogo = styled.div`
     }
   }
 `;
-const Content = styled.div`
+const LeftWrapper = styled.div`
   display: flex;
   flex-direction: column;
 `;
+const Content = styled.div`
+  height: 27vw;
+  width: 34vw;
+  margin-left: 3vw;
+  margin-top: 2.5vw;
+  display: flex;
+  font-size: 2vw;
+  font-weight: 700;
+  flex-direction: column;
+  div {
+    margin-top: 1vw;
+    &:after {
+      content: "";
+      display: block;
+      width: 35vw;
+      border-bottom: 3px solid black;
+      margin: 0.5vw auto;
+      margin-top: 1.5vw;
+    }
+  }
+  div:nth-child(1) {
+    font-size: 3vw;
+  }
+`;
+const Image = styled.img`
+  height: 20vw;
+  width: 18vw;
+  margin-left: 8vw;
+  margin-top: 5vw;
+`;
 const BottomWrapper = styled.div``;
+const EmailInput = styled.input`
+  position: relative;
+  width: 19vw;
+  height: 3vw;
+  background: white;
+  border-radius: 30px;
+  flex-direction: column;
+  border: 0.1vw solid black;
+  transform: rotate(3deg);
+  margin-left: 3vw;
+  margin-top: 1vw;
+`;
+const ContentInput = styled.input`
+  position: relative;
+  width: 19vw;
+  height: 13vw;
+  background: white;
+  border-radius: 30px;
+  flex-direction: column;
+  border: 0.1vw solid black;
+  transform: rotate(3deg);
+  margin-left: 2.5vw;
+  margin-top: 1vw;
+`;
+const SendBtn = styled.button`
+  position: relative;
+  width: 12vw;
+  height: 3vw;
+  background: white;
+  border-radius: 30px;
+  flex-direction: column;
+  border: 0.1vw solid black;
+  transform: rotate(3deg);
+  margin-left: 6vw;
+  margin-top: 1.5vw;
+  align-items: center;
+  div {
+    margin-top: 0.8vw;
+  }
+`;
 const About = () => {
   return (
     <Wrapper>
-      <Content>
+      <LeftWrapper>
         <TopWrapper>
           <ProjectLogo>
             <p>ABOUT</p>
@@ -91,10 +178,28 @@ const About = () => {
           </HomeLogo>
         </TopWrapper>
         <BottomWrapper>
-          <Big></Big>
+          <Introduction>
+            <Image src="https://image.shutterstock.com/image-photo/competition-forest-trees-sunlight-necessary-260nw-1186179847.jpg" />
+            <Content>
+              <div>안영준</div>
+              <div>이메일</div>
+              <div>깃허브</div>
+              <div>전화번호</div>
+              <div>학력</div>
+            </Content>
+          </Introduction>
         </BottomWrapper>
-      </Content>
-      <Qustion />
+      </LeftWrapper>
+      <Qustion>
+        <Questiontitle>Question</Questiontitle>
+        <QuestionContent>Username or email address</QuestionContent>
+        <EmailInput />
+        <QuestionContent>Content</QuestionContent>
+        <ContentInput />
+        <SendBtn>
+          <div>SEND</div>
+        </SendBtn>
+      </Qustion>
     </Wrapper>
   );
 };
