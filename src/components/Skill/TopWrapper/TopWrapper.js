f3679ad9f98082d637ac51b8b8505fe9c0e37288
jun <toinin@naver.com>
2021-04-19 17:18:56 +0900

    Add components - topwrapper - base

---
+++ src/components/Skill/TopWrapper/TopWrapper.js
@@ -0,0 +1,14 @@
+import * as S from "./style";
+const TopWrapper = () => {
+  return (
+    <S.TopWrapper>
+      <S.ProjectLogo>
+        <p>ABOUT</p>
+      </S.ProjectLogo>
+      <S.HomeLogo>
+        <p>HOME</p>
+      </S.HomeLogo>
+    </S.TopWrapper>
+  );
+};
+export default TopWrapper;
