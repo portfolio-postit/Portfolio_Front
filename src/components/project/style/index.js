6cd6107837a749ac67894d3f85cf935047ab9819
jun <toinin@naver.com>
2021-04-20 16:56:49 +0900

    FIx style - project height

--- src/components/project/style/index.js
+++ src/components/project/style/index.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 export const Wrapper = styled.div`
-  height: calc(90rem);
+  height: calc(60vw + (20vw * (${(props) => props.countProject / 3})));
   width: calc(100% - 20px);
   display: flex;
   background: #a6d5ff;
@@ -8,16 +8,17 @@ export const Wrapper = styled.div`
   border-left: 10px solid #1a0c0c;
   border-right: 10px solid #1a0c0c;
   @media only screen and (max-width: 768px) {
-    height: calc(40vw + (90vw * ${(props) => props.countProject}));
+    height: calc(50vw + (90vw * ${(props) => props.countProject}));
   }
 `;
 
 export const ProjectList = styled.div`
-  height: 60vw;
+  height: calc(30vw + (20vw * (${(props) => props.countProject / 3})));
   width: 100%;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   @media only screen and (max-width: 768px) {
     grid-template-columns: 1fr;
+    height: calc(10vw + (90vw * ${(props) => props.countProject}));
   }
 `;
