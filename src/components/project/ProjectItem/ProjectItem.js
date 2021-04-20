e4590c743af0a8c4ffba5f7b6757242f0747dd33
jun <toinin@naver.com>
2021-04-20 16:56:12 +0900

    Add components - projectitem props

--- src/components/project/ProjectItem/ProjectItem.js
+++ src/components/project/ProjectItem/ProjectItem.js
@@ -13,22 +13,31 @@ const ProjectItem = (props) => {
     },
     [dispatch]
   );
+  console.log(props);
   return (
     <>
       <S.ProjectItem onClick={() => modalOn(ProjectModal)}>
         <S.ProjectGroup>
-          <img src="springboot.png" />
+          <img src={process.env.REACT_APP_S3_URL + props.project.file_name} />
           <S.ProjectCotent>
             <S.ProjectTitle>{props.project.project_title}</S.ProjectTitle>
-            <S.ProjectMember>member = 1</S.ProjectMember>
-            <ProjectTag />
+            <S.ProjectMember>
+              member={props.project.member || 1}
+            </S.ProjectMember>
+            <ProjectTag projectTag={props.project.tag} />
           </S.ProjectCotent>
         </S.ProjectGroup>
         <S.ProgressName>Progress</S.ProgressName>
-        <S.Progress value="20" max="100"></S.Progress>
-        <S.ProgressName>Progress</S.ProgressName>
-        <S.Progress value="20" max="100"></S.Progress>
-        <S.GitURL href="https://github.com/Ahnyeongjun" target="blank">
+        <S.Progress
+          value={props.project.progress || 100}
+          max="100"
+        ></S.Progress>
+        <S.ProgressName>Contribution</S.ProgressName>
+        <S.Progress
+          value={props.project.contribution || 100}
+          max="100"
+        ></S.Progress>
+        <S.GitURL href={props.project.link} target="blank">
           GiT GO
         </S.GitURL>
       </S.ProjectItem>
