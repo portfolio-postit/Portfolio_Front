4dfae250105789b29ec68645655fd56b2aa069bf
jun <toinin@naver.com>
2021-04-20 18:34:24 +0900

    Add components - footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,11 @@
+import * as S from "./style";
+
+const Footer = () => {
+  return (
+    <S.Wrapper>
+      <S.Copyright> Copyright 2021. 안영준 all rights reserved.</S.Copyright>
+    </S.Wrapper>
+  );
+};
+
+export default Footer;
