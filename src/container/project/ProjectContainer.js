629c294dd90a805ac440dd9c17ad1d7586be5d19
jun <toinin@naver.com>
2021-04-20 13:58:23 +0900

    Add container - project  getProjectSaga

--- src/container/project/ProjectContainer.js
+++ src/container/project/ProjectContainer.js
@@ -1,10 +1,23 @@
+import React, { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProjectSaga } from "../../modules/redux/project";
 import HeaderContainer from "../header/headerContainer";
 import Project from "../../components/project/Project";
 const ProjectContainer = () => {
+  const project = useSelector((state) => state.project.project);
+  const dispatch = useDispatch();
+
+  const initMainSaga = useCallback(() => {
+    dispatch(getProjectSaga());
+  }, [dispatch]);
+
+  useEffect(() => {
+    initMainSaga();
+  }, []);
   return (
     <>
       <HeaderContainer />
-      <Project />
+      <Project project={project} />
     </>
   );
 };
