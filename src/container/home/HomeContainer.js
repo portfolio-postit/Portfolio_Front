23119f1d33126df23881c47268f3bcb217209b0f
jun <toinin@naver.com>
2021-04-20 18:35:23 +0900

    Add components - home - footer

--- src/container/home/HomeContainer.js
+++ src/container/home/HomeContainer.js
@@ -1,12 +1,13 @@
 import React from "react";
 import HeaderContainer from "../header/headerContainer.js";
 import BodyContainer from "./body/bodyContainer";
-
+import FooterContainer from "../footer/footerContainer";
 const HomeContainer = () => {
   return (
     <>
       <HeaderContainer />
       <BodyContainer />
+      <FooterContainer />
     </>
   );
 };
