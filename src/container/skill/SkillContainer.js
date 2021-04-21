00542d903dcf9074bd2f9f32309140848274f05b
jun <toinin@naver.com>
2021-04-21 22:17:21 +0900

    Add container - skill footer

--- src/container/skill/SkillContainer.js
+++ src/container/skill/SkillContainer.js
@@ -1,10 +1,12 @@
 import HeaderContainer from "../header/headerContainer";
 import Skill from "../../components/Skill/Skill";
+import FooterContainer from "../footer/footerContainer";
 const SkillContainer = () => {
   return (
     <>
       <HeaderContainer />
       <Skill />
+      <FooterContainer />
     </>
   );
 };
