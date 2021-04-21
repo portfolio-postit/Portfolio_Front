77487182a8f6becbe927856d0ba905d3e4f3f095
jun <toinin@naver.com>
2021-04-21 22:21:43 +0900

    Add style - skill

---
+++ src/components/Skill/style/index.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  height: 120%;
+  width: 100%;
+  background: #a6d5ff;
+  display: flex;
+`;
+export const LeftWrapper = styled.div`
+  display: flex;
+  width: 70vw;
+  height: 100%;
+  flex-direction: column;
+`;
+export const RightWrapper = styled.div`
+  display: flex;
+  width: 30vw;
+  height: 100%;
+  flex-direction: column;
+  margin-left: 5vw;
+`;
+export const Button = styled.div`
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url("skillPostit.png");
+  margin-top: 2.5vw;
+  height: 11.3vw;
+  width: 11vw;
+`;
