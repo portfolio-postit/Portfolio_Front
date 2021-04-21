89c06176c5b8d4ee4c9767d82c6e4c15628849b4
jun <toinin@naver.com>
2021-04-21 22:52:49 +0900

    Fix components - home - skillpostit name

--- src/components/home/postit/skillPostit/skillPostit.js
+++ src/components/home/postit/skillPostit/skillPostit.js
@@ -4,22 +4,22 @@ const skillPostit = (props) => {
   return (
     <>
       <S.SKillPostit>
-        <p class="margin40">Tool</p>
+        <p>Tool</p>
         <img src={props.url} class="size margin1" />
         <img src={props.url} class="margin2" height="40%" width="45%" />
       </S.SKillPostit>
       <S.SKillPostit>
-        <p class="framework">FrameWork</p>
+        <p>FrameWork</p>
         <img src={props.url} class="size margin1" />
         <img src={props.url} class="margin2" height="40%" width="45%" />
       </S.SKillPostit>
       <S.SKillPostit>
-        <p class="margin10">mostLanguage</p>
+        <p>Language</p>
         <img src={props.url} class="size margin1" />
         <img src={props.url} class="margin2" height="40%" width="45%" />
       </S.SKillPostit>
       <S.SKillPostit>
-        <p>subLanguage</p>
+        <p>etc</p>
         <img src={props.url} class="size margin1" />
         <img src={props.url} class="margin2" height="40%" width="45%" />
       </S.SKillPostit>
