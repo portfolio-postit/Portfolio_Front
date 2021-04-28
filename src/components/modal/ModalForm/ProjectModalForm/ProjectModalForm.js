8de20ed0fb24e1633d680cb6cf8de3f4e1fdbb4b
jun <toinin@naver.com>
2021-04-28 11:24:07 +0900

    Add components - modalform base

--- src/components/modal/ModalForm/ProjectModalForm/ProjectModalForm.js
+++ src/components/modal/ModalForm/ProjectModalForm/ProjectModalForm.js
@@ -1,18 +1,18 @@
 import * as S from "./style";
 import ModalForm from "../ModalForm";
 import LaftWrapper from "./LeftWrapper/LeftWrapper";
-const ProjectModalForm = () => {
+const ProjectModalForm = (props) => {
   return (
     <ModalForm>
-      <S.ProjectTitle>개발하자개발하자</S.ProjectTitle>
+      <S.ProjectTitle>{props.project.project_title}</S.ProjectTitle>
       <S.Wrapper>
         <LaftWrapper />
         <S.RightWrapper>
           <S.Essential>
-            <div>개발기간 : 2020 ~ 2020. (3개월)</div>
+            <div>{props.project.period}</div>
           </S.Essential>
           <S.Essential>
-            <div>개발인원 : 4명</div>
+            <div>개발인원: {props.project.member}명</div>
           </S.Essential>
           <S.Free>
             <div>내용 및 특징</div>
@@ -24,11 +24,11 @@ const ProjectModalForm = () => {
             <div>개발 상황</div>
             <S.ProgressWrapper>
               <p>진행도</p>
-              <S.Progress value="20" max="100" />
+              <S.Progress value={props.project.progress} max="100" />
             </S.ProgressWrapper>
             <S.ProgressWrapper>
-              <p>진행도</p>
-              <S.Progress value="20" max="100" />
+              <p>관여도</p>
+              <S.Progress value={props.project.contribution} max="100" />
             </S.ProgressWrapper>
           </S.Free>
           <S.Free>
