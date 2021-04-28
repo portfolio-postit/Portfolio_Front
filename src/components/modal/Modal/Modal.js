0a585a544c5d1b239c41f9fd134ea5bfbf97e4b1
jun <toinin@naver.com>
2021-04-28 11:23:05 +0900

    Fix style - modalCloseBtn

--- src/components/modal/Modal/Modal.js
+++ src/components/modal/Modal/Modal.js
@@ -2,13 +2,15 @@ import React from "react";
 import * as S from "./style";
 
 const Modal = (props) => {
-	const { children } = props;
+  const { children } = props;
 
-	const PreventModalOff = (e) => {
-		e.stopPropagation();
-	};
+  const PreventModalOff = (e) => {
+    e.stopPropagation();
+  };
 
-	return <S.Container onClick={(e) => PreventModalOff(e)}>{children}</S.Container>;
+  return (
+    <S.Container onClick={(e) => PreventModalOff(e)}>{children}</S.Container>
+  );
 };
 
 export default Modal;
--- src/components/modal/ModalCloseBtn/style/index.js
+++ src/components/modal/ModalCloseBtn/style/index.js
@@ -3,13 +3,17 @@ import styled from "styled-components";
 export const Container = styled.button`
   position: absolute;
   display: flex;
-  top: 20px;
-  right: 20px;
-  width: 39px;
-  height: 39px;
+  top: 1vw;
+  right: 1.5vw;
+  width: 4vw;
+  height: 4vw;
   border: none;
-  background: ${(props) => `url(${props.img}) no-repeat`};
+  background: ${(props) => `no-repeat center/80% url(${props.img}) `};
 
+  @media only screen and (max-width: 768px) {
+    width: 4vw;
+    height: 4vw;
+  }
   &:hover {
     cursor: pointer;
   }
