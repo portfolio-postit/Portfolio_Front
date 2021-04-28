171946839ea27f74c62b34a29f4acaa95697f92d
jun <toinin@naver.com>
2021-04-28 12:06:58 +0900

    Add style - modalform

--- src/components/modal/ModalForm/ProjectModalForm/style/index.js
+++ src/components/modal/ModalForm/ProjectModalForm/style/index.js
@@ -31,11 +31,14 @@ export const Essential = styled.div`
   font-weight: 700;
   margin-bottom: 1vw;
 `;
-export const Free = styled.div`
+export const ContentWrapper = styled.div`
   display: flex;
+  overflow: hidden;
   flex-direction: column;
   font-size: 1vw;
   font-weight: 500;
+  height: 6vw;
+  width: 37vw;
   margin-bottom: 1vw;
   p {
     margin-top: 0.5vw;
@@ -45,8 +48,36 @@ export const Free = styled.div`
     font-weight: 700;
   }
 `;
+export const SituationWrapper = styled.div`
+  display: flex;
+  overflow: hidden;
+  flex-direction: column;
+  font-size: 1vw;
+  font-weight: 500;
+  height: 8vw;
+  width: 37vw;
+  margin-bottom: 1vw;
+  p {
+    margin-top: 0.5vw;
+  }
+  div {
+    font-size: 1.5vw;
+    font-weight: 700;
+  }
+`;
+export const ImageWrapper = styled.div`
+  display: flex;
+  img {
+    height: 6vw;
+    width: 6.5vw;
+    margin-top: 5%;
+    margin-left: 10%;
+  }
+`;
+
 export const ProgressWrapper = styled.div`
   display: flex;
+  width: 37vw;
   p {
     margin-top: 1vw;
     font-size: 1vw;
@@ -54,8 +85,11 @@ export const ProgressWrapper = styled.div`
   }
 `;
 export const Progress = styled.progress`
+  @media only screen and (max-width: 768px) {
+    width: 28vw;
+  }
   margin-top: 0.5vw;
-  width: 30vw;
+  width: 32vw;
   height: 2vw;
   margin-left: 1.5vw;
   margin-bottom: 1px;
